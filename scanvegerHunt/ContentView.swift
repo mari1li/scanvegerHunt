d26fb42a86f0624d8f5a3e0767fd0c980e47f92f
marili1 <linh.truongngocmy@gmail.com>
2024-02-12 18:42:56 -0800

    Initial Commit

---
+++ scanvegerHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scanvegerHunt
+//
+//  Created by Linh Ngoc My Truong on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scanvegerHunt/scanvegerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  scanvegerHuntApp.swift
+//  scanvegerHunt
+//
+//  Created by Linh Ngoc My Truong on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct scanvegerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
